3a3ca72deb4acebb7a66f21019e4fc1b42cb98c5
BakaFT <bakaft@outlook.com>
2023-08-03 17:36:20 +0800

    Fix Xhr hooking

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { hookXhr, wrap_method, hookEmber } from './util'
 
 export function init(context) {
     // 尽早完成对XHR的Hook，所以选择第一个能够Hook的RCP
-    context.rcp.postInit('rcp-common-libs', api => {
+    context.rcp.postInit('rcp-fe-lol-l10n', api => {
         hookXhr()
     })
     // 尽早完成对Ember的Hook，所以选择在Ember-libs加载完毕后立刻进行
